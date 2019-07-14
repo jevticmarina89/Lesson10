1beef87d225844f071f01fb4a11363331fc08ba6
jevticmarina89 <marina.jevtic89@gmail.com>
2019-07-15 01:58:05 +0200

    Testing a secret code (in secret).

--- test/exercises/EncryptorTest.java
+++ test/exercises/EncryptorTest.java
@@ -19,5 +19,11 @@ class EncryptorTest {
 	
 	// Exercise: decrypt this sentence: Nkxg"nqpi"cpf"rtqurgt#
 	
+	@Test
+	void testDecryptSentence() throws Exception {
+		assertEquals("Live long and prosper!", Encryptor.decrypt("Nkxg\"nqpi\"cpf\"rtqurgt#"));
+		
+	}
+	
 	
 }
