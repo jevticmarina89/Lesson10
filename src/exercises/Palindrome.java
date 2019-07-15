cc20ce18ff59be011145bbfc4d722a55aa65afe3
jevticmarina89 <marina.jevtic89@gmail.com>
2019-07-15 02:00:00 +0200

    Making a palindrome.

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -1,12 +1,26 @@
 package exercises;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import examples.FileHelper;
 
 public class Palindrome {
 
+	List<String> words = new ArrayList<String>();
+
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
 	}
-}
+
+	public boolean wordExists(String s) {
+		Palindrome p = new Palindrome();
+		words = p.loadWords();
+		return words.contains(s);
+
+	}
+
+	public boolean wordIsPalindrome(String s) {
+		return wordExists(new StringBuilder(s).reverse().toString()) && wordExists(s); 
+			
+		
+}}
